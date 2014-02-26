a3cf7a09837e458dc662cbfbf0e14154b2ed2c7c
xieyu33333 <xieyu33333@gmail.com>
2014-02-27 07:03:56 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpensourceVote::Application.config.secret_key_base = '60a3079526b3f7c8af11d3fda620cdb710643fb2bd382c90f0d9e9b1e25b370fe75a0d72402a1ce6c5ff19d98b6a174a5676567ee3c12dfb7fa15928a9ed00b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpensourceVote::Application.config.session_store :cookie_store, key: '_Opensource_vote_session'
